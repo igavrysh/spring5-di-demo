890e649c46035a2dccb29f1cf8fe9b844af68ea2
Ievgen Gavrysh <ievgen_gavrysh@epam.com>
2019-07-15 07:50:33 -0700

    Ievgen solution

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -17,8 +17,8 @@ public class DiDemoApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 
 		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
+		//System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 }
--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,5 +1,7 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Service;
 @Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
 
+    @Autowired
+    @Qualifier("greetingRepositoryImpl")
     private GreetingRepository greetingRepository;
 
     @Override
